4b7bf7b7b685ff37f97d2c55844aebd605cae2b7
vignesh <vignesh0025@gmail.com>
2015-12-07 17:19:02 +0530

    Update INSTALL Script

--- Tools/install.sh
+++ Tools/install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-source $(wget -O - https://gist.githubusercontent.com/vignesh0025/7fda7c4a3416a78d62d1/raw/89a6f9d6378458ec52e24d4a9bc39cf92e6552b5/shell_colors.sh)
 
 wget -O /usr/local/cdb.py https://raw.githubusercontent.com/vignesh0025/ShellBookmark/master/Scripts/cdb.py
 wget -O /usr/local/cdb.sh https://raw.githubusercontent.com/vignesh0025/ShellBookmark/master/Scripts/cdb.sh
 
-echo "Add the following line to the end of .bashrc / .zshrc "
-echo "$UBLu source /usr/local/cdb.sh"
+python -c "print '\033[92m Please read the README.md at \033[4mhttps://github.com/vignesh0025/ShellBookmark\033[0m'"
+python -c "print '\033[91m Important: add the following line to ~/.zshrc or  ~/.bashrc or your preferred configuration file '"
+python -c "print '\033[95m \033[1m source /usr/local/cdb.sh'"
 
 
 
