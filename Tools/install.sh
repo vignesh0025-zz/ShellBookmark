977004c40adc2969e2a7576337e2abaf13123d20
vignesh <vignesh0025@gmail.com>
2015-12-07 16:34:34 +0530

    Update INSTALL Script

--- Tools/install.sh
+++ Tools/install.sh
@@ -3,8 +3,8 @@
 wget -O /usr/local/cdb.py https://raw.githubusercontent.com/vignesh0025/ShellBookmark/master/Scripts/cdb.py
 wget -O /usr/local/cdb.sh https://raw.githubusercontent.com/vignesh0025/ShellBookmark/master/Scripts/cdb.sh
 
-chmod +x https://raw.githubusercontent.com/vignesh0025/ShellBookmark/master/Scripts/cdb.py
-chmod +x https://raw.githubusercontent.com/vignesh0025/ShellBookmark/master/Scripts/cdb.sh
+chmod +x /usr/local/cdb.py
+chmod +x /usr/local/cdb.sh
 
 echo "Add the following line to the end of .bashrc / .zshrc "
 echo "source /usr/local/cdb.sh"
