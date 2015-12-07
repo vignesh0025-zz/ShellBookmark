31ac5f032a412e578163e386710997ac128c6646
vignesh <vignesh0025@gmail.com>
2015-12-07 16:44:55 +0530

    Update INSTALL Script

--- Tools/install.sh
+++ Tools/install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+source $(wget -O - https://gist.githubusercontent.com/vignesh0025/7fda7c4a3416a78d62d1/raw/89a6f9d6378458ec52e24d4a9bc39cf92e6552b5/shell_colors.sh)
+
 wget -O /usr/local/cdb.py https://raw.githubusercontent.com/vignesh0025/ShellBookmark/master/Scripts/cdb.py
 wget -O /usr/local/cdb.sh https://raw.githubusercontent.com/vignesh0025/ShellBookmark/master/Scripts/cdb.sh
 
 echo "Add the following line to the end of .bashrc / .zshrc "
-echo "source /usr/local/cdb.sh"
+echo "$UBLu source /usr/local/cdb.sh"
 
 
 
